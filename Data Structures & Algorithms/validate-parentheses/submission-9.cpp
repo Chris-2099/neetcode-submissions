535f65bb3189aed6bb3f62d57e0d332d7ec74c40
Chris Johnson <139840165+Chris-2099@users.noreply.github.com>
2026-07-20 21:49:17 -0700

    Add: validate-parentheses - submission-9

---
+++ Data Structures & Algorithms/validate-parentheses/submission-9.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        if(s.size() < 2) return false;
+
+        stack<char> st;
+        for(char c : s){
+            if(c == ')' && !st.empty()){
+                if(st.top() != '(') return false;
+                st.pop();
+            } else if(c == '}' && !st.empty()){
+                if(st.top() != '{') return false;
+                st.pop();
+            } else if(c == ']' && !st.empty()){
+                if(st.top() != '[') return false;
+                st.pop();
+            } else {
+                st.push(c);
+            }
+        }
+
+        if(!st.empty()) return false;
+        return true;
+    }
+};
