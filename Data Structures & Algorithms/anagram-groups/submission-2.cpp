516157337a32f2f607f27f1f1ef258683980aff1
Chris Johnson <139840165+Chris-2099@users.noreply.github.com>
2026-08-09 10:06:00 -0700

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    // group all anagrams together into sublists and return
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> res;
+        for (const auto& s : strs) {
+            string sortedS = s;
+            sort(sortedS.begin(), sortedS.end());
+            res[sortedS].push_back(s);
+        }
+        vector<vector<string>> result;
+        for (auto& pair : res) {
+            result.push_back(pair.second);
+        }
+        return result;
+    }
+};
